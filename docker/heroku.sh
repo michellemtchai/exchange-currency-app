20842d1998ecc5a6764d3715dc8e7739bada938e
michellemtchai <michelle.chai@mail.utoronto.ca>
2022-06-07 01:51:36 -0400

    create rb file

--- docker/heroku.sh
+++ docker/heroku.sh
@@ -2,12 +2,6 @@
 
 # install packages
 REACT_APP_SERVER_PORT=$PORT npx react-scripts build
-echo "files built"
 
 # copy files
 mv -v ./build/* ../server/public/
-echo "files moved"
-
-# move to server
-cd ../server
-echo "moved to server"
