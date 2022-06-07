d81c1bf1d4fe85071855fc07b5f2c8632f43d7f2
michellemtchai <michelle.chai@mail.utoronto.ca>
2022-06-07 01:44:27 -0400

    change script

--- docker/heroku.sh
+++ docker/heroku.sh
@@ -1,10 +1,12 @@
 #! /bin/sh
 
 # install packages
-yarn --silent
-REACT_APP_SERVER_PORT=$PORT yarn build
+REACT_APP_SERVER_PORT=$PORT react-scripts build
 echo "files built"
 
 # copy files
-mv ./build/* ../server/public/
+mv -v ./build/* ../server/public/
 echo "files moved"
+
+# move to server
+cd ../server
