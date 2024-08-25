0568f47d9044acf074fdf2be556c594ef97d01c8
michellemtchai <michellemtchai@gmail.com>
2024-08-25 14:21:46 -0400

    add i18n fallsbacks

--- server/config/application.rb
+++ server/config/application.rb
@@ -33,5 +33,8 @@ module Api
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = false
+
+    # sets the fallback to the default local
+    config.i18n.fallbacks = true
   end
 end
