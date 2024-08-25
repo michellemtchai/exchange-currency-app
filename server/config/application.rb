ed4b1c46325f4a2925160123d92b3e936a1e543f
michellemtchai <michellemtchai@gmail.com>
2024-08-25 15:48:48 -0400

    update rails version

--- server/config/application.rb
+++ server/config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Api
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 7.0
     config.assets.precompile = []
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
